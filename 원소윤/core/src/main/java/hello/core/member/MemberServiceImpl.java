fe14566ba34120bd3c7716d45d710acfe2f2aa95
Soyun Won <soyun9902@ewhain.net>
2022-07-26 23:53:53 +0900

    섹션 6. 컴포넌트 스캔과 의존관계 자동 주입 시작하기

--- 원소윤/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ 원소윤/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,14 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
+    @Autowired //의존관계를 자동으로 주입해서 연결
     public MemberServiceImpl(MemberRepository memberRepository) { //생성자를 통해 뭐가 들어갈지 결정
         this.memberRepository = memberRepository;
     }
